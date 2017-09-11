9b0660735767edeb810b24264acc3fdebed195b5
codeAB <hackerjieson@gmail.com>
2017-09-11 17:20:25 +0800

    modify

--- resources/views/web/shops.blade.php
+++ resources/views/web/shops.blade.php
@@ -2,11 +2,17 @@
 @section("title","徒步商城")
 @section("css")
     <style>
+        .toppic{
+            width:100%;height:335px;background-size:cover;background-position:center;background-repeat: no-repeat;
+            position: relative;
+        }
     </style>
 @stop
 
 @section("body")
     @include("web.header")
+    <div class="toppic" style="background-image: url(/admin/data/images/1505009109.jpg);" >
+    </div>
     <div class="content">
         <div style="font-size: 18px;color: #999;margin:30px 0">
             <a style="color: #999;" href="/">首页</a>
