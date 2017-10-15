fa0f4f589fd7a26c271023d4138de15502dba9e6
codeAB <hackerjieson@gmail.com>
2017-10-15 16:25:19 +0800

    modify

--- public/web/js/common.js
+++ public/web/js/common.js
@@ -112,7 +112,7 @@ function otherlogin(openid,type) {
                 "oauth_consumer_key":localStorage.getItem("101428001"),
             }, "json", "GET").success(function(s){
                 localStorage.setItem("qqdata",s.data);
-                $("#myModalLabel").append("<span>欢迎 <span style='color:red;font-weight:bold'>"+s.data.nickname+"</span> <初次使用请绑定手机号码></初次使用请绑定手机号码></span>")
+                $("#myModalLabel").append("<span>欢迎 <span style='color:red;font-weight:bold'>"+s.data.nickname+"</span>初次使用请绑定手机号码></span>")
                 $("#qqlogin").modal("show");
             })
         }
