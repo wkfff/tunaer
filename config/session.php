80a86de10dbab80252133d42f67302bb47e8ddd0
codeAB <hackerjieson@gmail.com>
2017-10-16 11:13:25 +0800

    modify

--- config/session.php
+++ config/session.php
@@ -29,9 +29,9 @@ return [
     |
     */
 
-    'lifetime' => 120,
+//    'lifetime' => 120,
 
-    'expire_on_close' => false,
+    'expire_on_close' => true,
 
     /*
     |--------------------------------------------------------------------------
