56ba67dee44598df328ce0b53e9bf5f6abed66e4
codeAB <hackerjieson@gmail.com>
2017-10-17 16:12:56 +0800

    modify

--- resources/views/admin/updatetubu.blade.php
+++ resources/views/admin/updatetubu.blade.php
@@ -44,7 +44,7 @@
                     <input type="text" value="{{$tubu->title}}" placeholder="主题" name="title" style="width: 503px !important;margin-top:10px;" name="title" ><br>
                     <input type="text" value="{{$tubu->howlong}}" placeholder="几天" name="howlong" >
                     <input type="text" value="{{$tubu->mudidi}}" placeholder="目的地" name="mudidi" ><br>
-                    <input type="text" value="{{$tubu->startday}}" placeholder="出发时间" name="startday" >
+                    <input type="text" value="{{$tubu->startday}}" placeholder="出发时间 格式:2017-12-15" name="startday" >
                     <input type="text" value="{{$tubu->endday}}" placeholder="返回时间" name="endday" ><br>
                     <input type="text" value="{{$tubu->jihedidian}}" placeholder="集合地点,多个集合地点使用#号隔开" name="jihedidian" >
                     <input type="text" value="{{$tubu->jihetime}}" placeholder="集合时间" name="jihetime" ><br>
