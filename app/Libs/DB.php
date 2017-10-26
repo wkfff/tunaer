a2f6b9865eb6d91e1312daf36c8a39ad774742c0
codeAB <hackerjieson@gmail.com>
2017-10-26 11:38:45 +0800

    modify

--- app/Libs/DB.php
+++ app/Libs/DB.php
@@ -9,7 +9,7 @@ $dbconfig = array(
     'DB_HOST'   => 'localhost', // 服务器地址
     'DB_NAME'   => 'tunaer', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => '123456', // 密码
+    'DB_PWD'    => 'abc123!@#', // 密码
     'DB_PORT'   => 3306, // 端口
     'DB_CHARSET'=> 'utf8', // 字符集
     
