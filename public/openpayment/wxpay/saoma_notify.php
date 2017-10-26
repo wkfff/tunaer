db7891463d6035b946509f9382bd75485f0b0cd0
codeAB <hackerjieson@gmail.com>
2017-10-26 15:46:36 +0800

    modify

--- public/openpayment/wxpay/saoma_notify.php
+++ public/openpayment/wxpay/saoma_notify.php
@@ -8,8 +8,6 @@ foreach ($postObj as $key => $value) {
 $status = $arr['result_code'];
 require_once dirname(__FILE__) . "/../../../app/Libs/DB.php";
 $handle = DB::getInstance();
-file_put_contents(dirname(__file__)."/log.php","111111111111111",FILE_APPEND);
-file_put_contents(dirname(__file__)."/log.php","****************************",FILE_APPEND);
 file_put_contents(dirname(__file__)."/log.php",file_get_contents("php://input"),FILE_APPEND);
 if(trim($status) == "SUCCESS") {
 
@@ -20,23 +18,27 @@ if(trim($status) == "SUCCESS") {
     $type = $tmparr[2];
 
     $sql = " insert into payment (paytype,money,orderid) values ('wx_saoma','".$money."','".$out_trade_no."') ";
-    $handle->query($sql);
+    file_put_contents(dirname(__file__)."/log.php","#".$sql.'#',FILE_APPEND);
+    $handle->excute($sql);
 
     if( $type == 'tubu' ) {
+        file_put_contents(dirname(__file__)."/log.php","#tubu#",FILE_APPEND);
         $sql = " update tubuorder set orderid='".$out_trade_no."' where id= ".$order_id;
+        file_put_contents(dirname(__file__)."/log.php","#".$sql."#",FILE_APPEND);
         $res = $handle->excute($sql);
     }else{
+        file_put_contents(dirname(__file__)."/log.php","#other#",FILE_APPEND);
         $sql = " update shoporder set orderid='".$out_trade_no."' where id= ".$order_id;
         $res = $handle->excute($sql);
     }
-
+    file_put_contents(dirname(__file__)."/log.php","#over#",FILE_APPEND);
     $xml = "
     <xml>
       <return_code><![CDATA[".$status."]]></return_code>
       <return_msg><![CDATA[OK]]></return_msg>
     </xml>";
     echo $xml;
-		
+    file_put_contents(dirname(__file__)."/log.php","#end#",FILE_APPEND);
 }else {
     echo "fail";	//请不要修改或删除
 
