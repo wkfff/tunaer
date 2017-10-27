6897fd5acb7e5d1834d8eda9a50c611d9168ba63
codeAB <hackerjieson@gmail.com>
2017-10-27 10:43:12 +0800

    modify

--- public/openpayment/alipay_pc/pagepay/pagepay.php
+++ public/openpayment/alipay_pc/pagepay/pagepay.php
@@ -8,7 +8,9 @@
 require_once dirname(dirname(__FILE__)).'/config.php';
 require_once dirname(__FILE__).'/service/AlipayTradeService.php';
 require_once dirname(__FILE__).'/buildermodel/AlipayTradePagePayContentBuilder.php';
-
+echo "<pre>";
+    var_dump($_POST);
+    die;
     //商户订单号，商户网站订单系统中唯一订单号，必填
     $out_trade_no = trim($_POST['WIDout_trade_no']);
 
