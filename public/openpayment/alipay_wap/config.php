8508d2280d291563672435340994ed926ec85539
codeAB <hackerjieson@gmail.com>
2017-10-27 09:40:47 +0800

    modify

--- public/openpayment/alipay_wap/config.php
+++ public/openpayment/alipay_wap/config.php
@@ -1,7 +1,7 @@
 <?php
 $config = array (	
 		//应用ID,您的APPID。
-		'app_id' => "2088821550096237",
+		'app_id' => "2017102509515743",
 
 		//商户私钥，您的原始格式RSA私钥
 		'merchant_private_key' => "MIICXAIBAAKBgQDStcwlW8THTRtNYvjsjciOCancp+HQjBdsri2EXJoDEV0HXwpcwDgRNKQ5h15RkRYMePoQP9XDnunwxvB9WV5iVxSYohjO79SdGmuJWRSoRiZCDAZgFopkInO9tSEfTqgMaIp3ltQH0FtoLCCDwIj2wKdU147zG/2jpyB7kB0+SwIDAQABAoGAeLSqUNBkRqO2OzuyS5jhRfTlOMF3i8dk995DtupxU8aTm1BnmECJHdohJc+VSXaqwdRftAVNiLW1YDgmtjgG55W7Q95Z7Qp0Em7XgV9qazsp0Nne8XyOCsItyDDRjRU61FV8K+t0x0ItuOkyG3PS7THlWzSi+muv1s09f+uq13ECQQD+Jb7rb9Gbbe2d3aUGr1pQAHc+laDIOXSBrRGMGClme6LDHUDfRdaLEFnL5fyLgGM1BLCydW2rBPjxmR8UWGVzAkEA1D7+vhLXulzRw/YvSDB3utSErDvplFJYbObCbTKt0BAJHc9XTM021cxf/4DOO5V13vwzAbOpMd4zFeQCAoZNyQJAYKhAZVCAuoljbr/mTJWSVozmzSGhJaVcXxlQbCSgUj7BV02f54qrHvaYAEk29GNe18Ix5Z1tmTZp65Dd1iIMawJBAMr+VPXFX75F9/nMAvywHlL53so7ovLQrvG44ks+JS7Rp2ZzX7N8se1ZQpdDwfsKv2k9HCuMgU3IjmNpZuKzQMECQAIG5O4KFsQOuOx0DNpZrWofnepEfhxTn58DivXiEVL+LuqUgr7zcL46EoPwIfJsY56ux7/H9f5Ov7QdKdXkBEU=",
@@ -22,7 +22,7 @@ $config = array (
 		'gatewayUrl' => "https://openapi.alipay.com/gateway.do",
 
 		//支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
-		'alipay_public_key' => "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB",
+		'alipay_public_key' => "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDI6d306Q8fIfCOaTXyiUeJHkrIvYISRcc73s3vF1ZT7XN8RNPwJxo8pWaJMmvyTn9N4HQ632qJBVHf8sxHi/fEsraprwCtzvzQETrNRwVxLO5jVmRGi60j8Ue1efIlzPXV9je9mkjzOmdssymZkh2QhUrCmZYI/FCEa3/cNMW0QIDAQAB",
 		
 	
 );
