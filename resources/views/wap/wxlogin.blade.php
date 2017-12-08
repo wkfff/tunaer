f88c9e0894cef159930a155ef6d2f9d851a9170b
caojie <hackerjieson@gmail.com>
2017-12-08 17:42:20 +0800

    modify

--- resources/views/wap/wxlogin.blade.php
+++ resources/views/wap/wxlogin.blade.php
@@ -54,7 +54,7 @@
             wxdata['gender'] = "{{$userinfo->sex=='1' ? '男':'女'}}";
             wxdata['year'] = "1990";
             wxdata['city'] = "{{$userinfo->country.'-'.$userinfo->province.'-'.$userinfo->city}}";
-{{--            wxdata['figureurl_qq_2'] = "{{$userinfo->headimgurl}}";--}}
+            wxdata['figureurl_qq_2'] = "{{$userinfo->headimgurl}}";
             localStorage.setItem('qqdata',JSON.stringify(wxdata));
             localStorage.setItem("wx_openid","{{$userinfo->openid}}");
             alert("3");
