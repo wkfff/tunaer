798bc2d0e0e40e2cdd7dd951e22f1e03729f2e5c
caojie <hackerjieson@gmail.com>
2017-12-09 20:13:50 +0800

    modify

--- resources/views/wap/wxlogin.blade.php
+++ resources/views/wap/wxlogin.blade.php
@@ -51,11 +51,11 @@
 
             var wxdata = new Array();
             wxdata['nickname'] = "{{$userinfo->nickname}}";
-            wxdata['gender'] = "{{$userinfo->sex=='1' ? '男':'女'}}";
-            wxdata['year'] = "1990";
-            wxdata['city'] = "{{$userinfo->country.'-'.$userinfo->province.'-'.$userinfo->city}}";
-            wxdata['figureurl_qq_2'] = "{{$userinfo->headimgurl}}";
-            localStorage.setItem('qqdata',JSON.stringify(wxdata));
+            {{--wxdata['gender'] = "{{$userinfo->sex=='1' ? '男':'女'}}";--}}
+            {{--wxdata['year'] = "1990";--}}
+            {{--wxdata['city'] = "{{$userinfo->country.'-'.$userinfo->province.'-'.$userinfo->city}}";--}}
+            {{--wxdata['figureurl_qq_2'] = "{{$userinfo->headimgurl}}";--}}
+            {{--localStorage.setItem('qqdata',JSON.stringify(wxdata));--}}
             alert(JSON.stringify(wxdata));
             localStorage.setItem("wx_openid","{{$userinfo->openid}}");
             $("#wxlogin").modal("show");
