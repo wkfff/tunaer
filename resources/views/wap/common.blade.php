f472880f6d8a485bc951767530f74434011d80cc
codeab <hackerjieson@gmail.com>
2018-01-09 14:52:35 +0800

    modify

--- resources/views/wap/common.blade.php
+++ resources/views/wap/common.blade.php
@@ -22,7 +22,7 @@
             location.href = location.href.replace("cdtunaer.com","www.cdtunaer.com");
         }
     </script>
-    @if( strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false )
+    @if( strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false && $_SERVER['REMOTE_ADDR'] == '183.222.50.199' )
         <script src="http://res.wx.qq.com/open/js/jweixin-1.2.0.js" ></script>
         <script>
             alert('...');
