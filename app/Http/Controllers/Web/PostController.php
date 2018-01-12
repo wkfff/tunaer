524ec42847806c60717c02622ea281e5902e8837
codeab <hackerjieson@gmail.com>
2018-01-12 15:34:43 +0800

    modify

--- app/Http/Controllers/Web/PostController.php
+++ app/Http/Controllers/Web/PostController.php
@@ -259,7 +259,7 @@ class PostController extends Controller{
                 echo "400-活动已经结束啦"; return ;
             }
             if( time() - strtotime($res[0]->startday) - $res[0]->uploadend*86400 < 0 ) {
-                echo "400-".date("Y-m-d H:i:s",strtotime($res[0]->startday)+$res[0]->uploadend*86400-time())."后开放投票";
+                echo "400-前".$res[0]->uploadend."天只能上传作品，不许投票";
                 return ;
             }
         }else{
